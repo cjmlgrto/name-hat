77e49658876ef364df563f53ea37e8046db45845
Carlos Melegrito <cj@mlgrto.com>
2019-11-03 11:47:28 +1100

    Setup Twilio

--- src/Members.js
+++ src/Members.js
@@ -2,18 +2,22 @@ const Members = [
     {
         ID: 0,
         name: 'A',
+        mobile: '+61000000000',
     },
     {
         ID: 1,
         name: 'B',
+        mobile: '+61000000000',
     },
     {
         ID: 2,
         name: 'C',
+        mobile: '+61000000000',
     },
     {
         ID: 3,
         name: 'D',
+        mobile: '+61000000000',
     },
 ]
 
