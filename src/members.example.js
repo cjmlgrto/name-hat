ea5f2c98d4c7423795060a88061cfc4a166d4d8a
Carlos Melegrito <cj@mlgrto.com>
2019-11-03 13:12:12 +1100

    Add demos

---
+++ src/members.example.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        ID: 0,
+        name: 'Person A',
+        mobile: '+00000000000'
+    },
+    {
+        ID: 1,
+        name: 'Person B',
+        mobile: '+00000000000'
+    },
+    {
+        ID: 2,
+        name: 'Person C',
+        mobile: '+00000000000'
+    },
+    {
+        ID: 3,
+        name: 'Person D',
+        mobile: '+00000000000'
+    }
+]
---
+++ tests/test-sms.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const mobile = process.env.TWILIO_MOBILE
+
+const testNumber = process.env.TEST_NUMBER
+
+const client = require('twilio')(accountSid, authToken);
+
+function testSMS() {
+    client.messages.create({
+        body: `This is a test`,
+        from: mobile,
+        to: testNumber
+    }).then(message => console.log(`SENT: ${message.sid} TO: ${testNumber}`));
+}
+
+testSMS();
